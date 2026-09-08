0edfb1d9239663abedb64f895b49a65e35d352f1
JoselysHernandez <hernandezjoselys0@gmail.com>
2026-09-08 17:07:53 -0300

    Lab

--- 09 - Lab - Preparación para analizar la insulina con Python.py
+++ 09 - Lab - Preparación para analizar la insulina con Python.py
@@ -1,35 +1,41 @@
-# Limpieza de preproinsulin-seq.txt mediante programación
-#import re
+# Store the human preproinsulin sequence in a variable called preproinsulin:
 
-#with open("preproinsulin-seq.txt", "r") as file:
-#    sequence = file.read()
- 
-#sequence = re.sub(r"ORIGIN|//|\d+|\s+", "", sequence)
- 
-#with open("preproinsulin-seq.txt", "w") as file:
-#    file.write(sequence)
+preproInsulin = "malwmrllpllallalwgpdpaaafvnqhlcgshlvealylvcgergffytpktr" \
+"reaedlqvgqvelgggpgagslqplalegslqkrgiveqcctsicslyqlenycn"
 
-### Obtención de la secuencia de proteínas de la insulina humana
+# Store the remaining sequence elements of human insulin in variables:
 
-with open("preproinsulin-seq.txt", "r") as file:
-    sequence = file.read()
+lsInsulin = "malwmrllpllallalwgpdpaaa"
+bInsulin = "fvnqhlcgshlvealylvcgergffytpkt"
+aInsulin = "giveqcctsicslyqlenycn"
+cInsulin = "rreaedlqvgqvelgggpgagslqplalegslqkr"
 
-lsinsulin = sequence[:24]
-binsulin = sequence[24:54]
-cinsulin = sequence[54:89]
-ainsulin = sequence[89:110]
+insulin = bInsulin + aInsulin
 
-with open("lsinsulin-seq-clean.txt", "w") as file:
-    file.write(lsinsulin)
-with open("binsulin-seq-clean.txt", "w") as file:
-    file.write(binsulin)
-with open("cinsulin-seq-clean.txt", "w") as file:
-    file.write(cinsulin)
-with open("ainsulin-seq-clean.txt", "w") as file:
-    file.write(ainsulin)
+# Printing "the sequence of human insulin" to console using successive print() 
+# commands:
+print("The sequence of human preproinsulin:")
+print(preproInsulin)
 
+# Printing to console using concatenated strings inside the print function (one-liner):
+print("The sequence of human insulin, chain a:", aInsulin)
 
-print(len(lsinsulin))
-print(len(binsulin))
-print(len(cinsulin))
-print(len(ainsulin))
+# Calculating the molecular weight of insulin  
+# Creating a list of the amino acid (AA) weights  
+aaWeights = {'A': 89.09, 'C': 121.16, 'D': 133.10, 'E': 147.13, 'F': 165.19,
+'G': 75.07, 'H': 155.16, 'I': 131.17, 'K': 146.19, 'L': 131.17, 'M': 149.21,
+'N': 132.12, 'P': 115.13, 'Q': 146.15, 'R': 174.20, 'S': 105.09, 'T': 119.12,
+'V': 117.15, 'W': 204.23, 'Y': 181.19}  
+# Count the number of each amino acids  
+aaCountInsulin = ({x: float(insulin.upper().count(x)) for x in ['A', 'C',
+'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T',
+'V', 'W', 'Y']})  
+# Multiply the count by the weights  
+molecularWeightInsulin = sum({x: (aaCountInsulin[x]*aaWeights[x]) for x in
+['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R',
+'S', 'T', 'V', 'W', 'Y']}.values())  
+print("The rough molecular weight of insulin: " +
+str(molecularWeightInsulin))
+
+molecularWeightInsulinActual = 5807.63
+print("Error percentage: " + str(((molecularWeightInsulin - molecularWeightInsulinActual)/molecularWeightInsulinActual)*100))
---
+++ 10 - Lab - Preparación para analizar la insulina con Python.py
@@ -0,0 +1,35 @@
+# Limpieza de preproinsulin-seq.txt mediante programación
+#import re
+
+#with open("preproinsulin-seq.txt", "r") as file:
+#    sequence = file.read()
+ 
+#sequence = re.sub(r"ORIGIN|//|\d+|\s+", "", sequence)
+ 
+#with open("preproinsulin-seq.txt", "w") as file:
+#    file.write(sequence)
+
+### Obtención de la secuencia de proteínas de la insulina humana
+
+with open("preproinsulin-seq.txt", "r") as file:
+    sequence = file.read()
+
+lsinsulin = sequence[:24]
+binsulin = sequence[24:54]
+cinsulin = sequence[54:89]
+ainsulin = sequence[89:110]
+
+with open("lsinsulin-seq-clean.txt", "w") as file:
+    file.write(lsinsulin)
+with open("binsulin-seq-clean.txt", "w") as file:
+    file.write(binsulin)
+with open("cinsulin-seq-clean.txt", "w") as file:
+    file.write(cinsulin)
+with open("ainsulin-seq-clean.txt", "w") as file:
+    file.write(ainsulin)
+
+
+print(len(lsinsulin))
+print(len(binsulin))
+print(len(cinsulin))
+print(len(ainsulin))
