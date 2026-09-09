6aa712c4425904b62e7edb72bc7ea970e19d9777
JoselysHernandez <hernandezjoselys0@gmail.com>
2026-09-09 12:38:46 -0300

    15 - Lan

---
+++ 15 - Lab - Uso del depurador.py
@@ -0,0 +1,11 @@
+# Exploración de las características básicas del depurador Python de AWS Cloud9
+name = "John"
+print("Hello " + name + ".")
+
+age = 40
+print(name + " is " + str(age) + " years old.")
+
+
+
+
+
